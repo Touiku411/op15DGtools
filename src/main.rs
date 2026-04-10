7906a73a9546b1efc638e9ce6c40bc45414e5f82
Touiku411 <tengyu081229@gmail.com>
2026-04-10 18:28:49 +0800

    finish jiuzhuan

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::{self, /*Read,*/ Write}, path::{Path}, process::Command, thread, time::Duration};
+use std::{fs, io::{self, Write}, path::Path, process::Command, thread::{self}, time::Duration};
 
 fn pause_terminal(msg: &str) {
                 print!("{}",msg);
@@ -7,10 +7,15 @@ fn pause_terminal(msg: &str) {
                 io::stdin().read_line(&mut temp).unwrap();
 }
 fn clear_screen() {
-        let _ = Command::new("clear").status();
+        if cfg!(target_os = "windows"){
+                let _ = Command::new("cmd").args(["/c","cls"]).status();
+        }
+        else {
+                let _ = Command::new("clear").status();
+        }
 }
 fn enter_choice() -> i32{
-        println!("       ---------ONEPLUS 15 升降級工具 (Rust版)---------");
+        println!("       ---------ONEPLUS 15 升降級工具 (Support Windows&Linux)---------");
         println!("\x1b[1;31m====================================================");
         println!("!僅限[一加15]使用，若使用其他機型導致變磚，一概不負責!");
         println!("!請將ota包更改名稱為update.zip，並放入ota資料夾下!");
@@ -40,7 +45,9 @@ fn enter_choice() -> i32{
 }
 
 fn environment() {
+        clear_screen();
         loop{
+                clear_screen();
                 println!("\n[一些實用指令]");
                 println!("1. adb devices");
                 println!("2. fastboot devices");
@@ -57,14 +64,16 @@ fn environment() {
                                 Command::new("adb")
                                 .arg("devices")
                                 .status()
-                                .expect("❌ 執行 adb 失敗，請確認已安裝 android-tools");
+                                .expect("執行 adb 失敗，請確認已安裝 android-tools");
+                                pause_terminal("按Enter鍵繼續...");
                         }
                         Ok(2) =>{
                                 println!("\n若什麼都沒顯示代表未連接，請確認處於 fastboot 模式...\n");
                                 Command::new("fastboot")
                                 .arg("devices")
                                 .status()
-                                .expect(" ❌ 執行 fastboot 失敗");
+                                .expect("執行 fastboot 失敗");
+                                pause_terminal("按Enter鍵繼續...");
                         }
                         Ok(3) => boot_to_bootloader(),
                         Ok(4) => boot_to_recovery(),
@@ -75,20 +84,18 @@ fn environment() {
 }
 fn boot_to_bootloader() {
         println!("\n正在重啟至 bootloader...\n");
-    
-        // let _ = 代表：「我知道這指令可能會報錯(例如手機不在adb模式)，但我不在乎，繼續執行」
-        // .args([...]) 可以直接傳入一個陣列，比拼接字串直覺太多了！
         let _ = Command::new("adb").args(["reboot", "bootloader"]).status();
         let _ = Command::new("fastboot").args(["reboot", "bootloader"]).status();
         println!("指令發送完畢。\n");
+        pause_terminal("按Enter鍵繼續...");
+
 }
 fn boot_to_recovery() {
         println!("\n正在重啟至 Recovery...\n");
-        
         let _ = Command::new("adb").args(["reboot", "recovery"]).status();
         let _ = Command::new("fastboot").args(["reboot", "recovery"]).status();
-        
         println!("指令發送完畢。\n");
+        pause_terminal("按Enter鍵繼續...");
 }
 fn clear_ota(){
         let dir = Path::new("ota");
@@ -103,7 +110,7 @@ fn clear_ota(){
                         pause_terminal("按 Enter 鍵繼續...");
                 }
                 else{
-                        println!("已取消刪除");
+                        pause_terminal("已取消刪除,按 Enter 鍵繼續...");
                 }
         }
         else{
@@ -114,7 +121,7 @@ fn clear_ota(){
 fn clear_images() {
         let dir = Path::new("images");
         if dir.exists() {
-                println!("確認刪除 images 資料夾內容?[y/n]");
+                print!("確認刪除 images 資料夾內容?[y/n]");
                 io::stdout().flush().unwrap();
                 let mut input =  String::new();
                 io::stdin().read_line(&mut input).unwrap();
@@ -124,7 +131,7 @@ fn clear_images() {
                         pause_terminal("按 Enter 鍵繼續...");
                 }
                 else{
-                        println!("已取消刪除");
+                        pause_terminal("已取消刪除,按 Enter 鍵繼續...");
                 }
         }
         else{
@@ -158,14 +165,12 @@ fn unpack() {
         else {
                 println!("ERROR: 解壓縮失敗，請確認 update.zip 是否損壞。");
                 pause_terminal("按 Enter 鍵繼續...");
-                clear_screen();
                 return;
         }
         if !bin.exists() {
                 println!("提取payload.bin失敗");
                 println!("確認檔案是否存在");
                 pause_terminal("按 Enter 鍵繼續...");
-                clear_screen();
                 return;
         }
         println!("開始解包payload.bin");
@@ -198,6 +203,10 @@ fn start(){
                         }
                         Ok(2) => {
                                 jiuzhuan();
+                                break;
+                        }
+                        Ok(3) =>{
+                                clear_screen();
                                 break;
                         }
                         _ => {
@@ -279,10 +288,95 @@ fn shengijanji(){
         }
         println!("\n請在手機選擇語言，並選擇格式化數據後重啟");
         pause_terminal("\n刷機完成，按任意鍵返回主頁");
+        clear_screen();
 }
 fn jiuzhuan(){
+        let pre_fwpartitions = [
+                "vbmeta", "vbmeta_system", "vbmeta_vendor", "vendor_boot", "init_boot",
+                "boot", "recovery", "modem", "bluetooth", "cpucp", "cpucp_dtb",
+                "dsp", "dtbo", "engineering_cdt", "featenabler", "oplus_sec",
+                "shrm", "splash", "uefi"
+        ];
+        let cow_partitions = [
+                 "system", "system_dlkm", "system_ext", "vendor", "product", "odm",
+                "my_bigball", "my_carrier", "my_engineering", "my_heytap", "my_manifest",
+                "my_product", "my_region", "my_stock", "odm_dlkm", "vendor_dlkm"
+        ];
+        let postfw_partitions = [
+                "aop", "aop_config", "devcfg", "hyp", "imagefv", "keymaster",
+                "oplusstanvbk", "qupfw", "tz", "uefisecapp", "abl", "xbl",
+                "xbl_config", "xbl_ramdump"
+        ];
+        let sys_partitions = [
+                "my_bigball", "my_carrier", "my_engineering", "my_heytap", "my_manifest",
+                "my_product", "my_region", "my_stock", "odm", "product", "system",
+                "system_dlkm", "system_ext", "vendor", "vendor_dlkm"
+        ];
 
-}
+        let img_dir = Path::new("images");
+        for s in pre_fwpartitions {
+                let img = format!("{}.img",s);
+                let img_path = img_dir.join(&img);
+                if img_path.exists() {
+                        for slot in ["a","b"] {
+                                let partition_name = format!("{}_{}",s,slot);
+                                let _ = Command::new("fastboot")
+                                .args(["flash",&partition_name])
+                                .arg(&img_path)
+                                .status();
+                        }   
+                }
+                else {
+                        if s == "cpucp_dtb" {
+                                println!("\n[略過] 未找到可選分區 {}，已自動忽略", img);
+                        }
+                }
+        }
+        println!("\n正在進入Fastboot模式，請勿動手機和電腦");
+        let _ = Command::new("fastboot").args(["reboot","fastboot"]).status();
+        println!("\n請等待10秒...");
+        thread::sleep(Duration::from_secs(10));
+        for s in cow_partitions{
+                for slot in ["a","b"]{
+                        let partition_name = format!("{}_{}-cow",s,slot);
+                        let _ = Command::new("fastboot")
+                        .args(["delete-logical-partition", &partition_name])
+                        .status();
+                }
+        }
+        for s in postfw_partitions {
+                let img = format!("{}.img",s);     
+                let img_path = img_dir.join(&img);
+                if img_path.exists() {
+                        for slot in ["a","b"] {
+                                let partition_name = format!("{}_{}",s , slot);
+                                let _ = Command::new("fastboot")
+                                .args(["flash", &partition_name])
+                                .arg(&img_path)
+                                .status();
+                                }
+                        }
+                        else{
+                                if s == "oplusstanvbk" {
+                                        println!("\n[略過] 未找到可選分區 {}，已自動忽略",img);
+                                }
+        
+                }
+        }
+         for s in sys_partitions{
+                let img = format!("{}.img",s);
+                let img_path = img_dir.join(img);
+                if img_path.exists() {
+                        let _ = Command::new("fastboot")
+                        .args(["flash",s])
+                        .arg(img_path)
+                        .status();
+                }
+        }
+        println!("\n請在手機選擇語言，並選擇格式化數據後重啟");
+        pause_terminal("\n刷機完成，按任意鍵返回主頁");
+        clear_screen();
+}       
 
 
 fn main() {
@@ -290,6 +384,7 @@ fn main() {
         let _ = fs::create_dir_all("tools");
         let _ = fs::create_dir_all("ota");
         loop {
+                clear_screen();
                 let choice = enter_choice();
                 match choice {
                         1 => println!("待刪"),
